cf14d1e3d89c68a81d99c17cb4d03d9552fc186e
AJP <allenatmc@gmail.com>
2016-02-21 19:13:12 -0600

    use and modify examples from mozilla's canvas animation tutorial

--- scripts/canvas1.js
+++ scripts/canvas1.js
@@ -1,21 +1,49 @@
-var canvas = document.getElementById('canvas1');
-var ctx = canvas.getContext('2d');
+var canvas1 = document.getElementById('canvas1');
 
-function draw() {
+var sun = new Image();
+var moon = new Image();
+var earth = new Image();
 
-    // ctx.fillRect(25,25,100,100);
-    // ctx.clearRect(45,45,60,60);
-    // ctx.fillRect(50,50,50,50);
-
-    for (var i=0;i<6;i++){
-    for (var j=0;j<6;j++){
-      ctx.fillStyle = 'rgb(' + Math.floor(255-42.5*i) + ',' + Math.floor(255-42.5*j) + ', 0)';
-      ctx.fillRect(j*25,i*25,25,25);
-    }
-  }
-
-
+function init(){
+  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
+  window.requestAnimationFrame(draw);
 }
 
+function draw() {
+  var ctx = canvas1.getContext('2d');
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0,0,300,300); // clear canvas
 
-draw();
+  ctx.fillStyle = 'rgba(0,0,0,0.4)';
+  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+  ctx.save();
+  ctx.translate(150,150);
+
+  // Earth
+  var time = new Date();
+  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+  ctx.translate(105,0);
+  ctx.fillRect(0,-12,50,24); // Shadow
+  ctx.drawImage(earth,-12,-12);
+
+  // Moon
+  ctx.save();
+  ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+  ctx.translate(28.5,0);
+  ctx.drawImage(moon,-3.5,-3.5);
+  ctx.restore();
+
+  ctx.restore();
+
+  ctx.beginPath();
+  // ctx.arc(150,150,105,0,Math.PI*2,false); // Earth orbit
+  // ctx.stroke();
+
+  ctx.drawImage(sun,0,0,300,300);
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
--- scripts/canvas2.js
+++ scripts/canvas2.js
@@ -1,11 +1,100 @@
-var canvas = document.getElementById('canvas2');
-var ctx = canvas.getContext('2d');
+var canvas2 = document.getElementById('canvas2');
 
-function draw() {
+function clock(){
+  var now = new Date();
+  var ctx = canvas2.getContext('2d');
+  ctx.save();
+  ctx.clearRect(0,0,400,400);
+  ctx.translate(200,200);
+  ctx.scale(1,1);
+  ctx.rotate(-Math.PI/2);
+  ctx.strokeStyle = "black";
+  ctx.fillStyle = "white";
+  ctx.lineWidth = 4;
+  ctx.lineCap = "round";
 
-    ctx.fillRect(25,25,100,100);
-    ctx.clearRect(45,45,60,60);
-    ctx.fillRect(50,50,50,50);
+  // Hour marks
+  ctx.save();
+  for (var i=0;i<12;i++){
+    ctx.beginPath();
+    ctx.rotate(Math.PI/6);
+    ctx.moveTo(150,0);
+    ctx.lineTo(175,0);
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Minute marks
+  ctx.save();
+  ctx.lineWidth = 2;
+  for (i=0;i<60;i++){
+    if (i%5!=0) {
+      ctx.beginPath();
+      ctx.moveTo(155,0);
+      ctx.lineTo(170,0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI/30);
+  }
+  ctx.restore();
+
+  var sec = now.getSeconds();
+  var min = now.getMinutes();
+  var hr  = now.getHours();
+  hr = hr>=12 ? hr-12 : hr;
+
+  ctx.fillStyle = "black";
+
+  // write Hours
+  ctx.save();
+  ctx.rotate( hr*(Math.PI/6) + (Math.PI/360)*min + (Math.PI/21600)*sec )
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(-20,0);
+  ctx.lineTo(80,0);
+  ctx.stroke();
+  ctx.restore();
+
+  // write Minutes
+  ctx.save();
+  ctx.rotate( (Math.PI/30)*min + (Math.PI/1800)*sec )
+  ctx.lineWidth = 8;
+  ctx.beginPath();
+  ctx.moveTo(-30,0);
+  ctx.lineTo(130,0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Write seconds
+  ctx.save();
+  ctx.rotate(sec * Math.PI/30);
+  ctx.strokeStyle = "teal";
+  ctx.fillStyle = "teal";
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-30,0);
+  ctx.lineTo(83,0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0,0,10,0,Math.PI*2,true);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(95,0,10,0,Math.PI*2,true);
+  ctx.stroke();
+  ctx.fillStyle = "rgba(0,0,0,0)";
+  ctx.arc(0,0,3,0,Math.PI*2,true);
+  ctx.fill();
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = 'teal';
+  ctx.arc(0,0,175,0,Math.PI*2,true);
+  ctx.stroke();
+
+  ctx.restore();
+
+  window.requestAnimationFrame(clock);
 }
 
-draw();
+window.requestAnimationFrame(clock);
